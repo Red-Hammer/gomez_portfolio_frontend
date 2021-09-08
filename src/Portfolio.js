bd28654cf22897b81bb4f57b8dc872150a9c473c
Brandon Sorrow <bsorrow@healthcarebluebook.com>
2021-09-08 18:30:30 -0500

    Added API fetching for Portfolio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,46 +1,51 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Gallery from 'react-grid-gallery';
+import {useState} from "react";
 
 
 export default function Portfolio() {
-    const IMAGES = [{
-        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 174,
-        caption: "After Rain (Jeshu John - designerspics.com)"
-    },
-        {
-            src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-            thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212,
-            tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-            caption: "Boats (Jeshu John - designerspics.com)"
-        },
 
-        {
-            src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-            thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
-        }]
+    // Trying this with Hooks
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [images, setImages] = useState([]);
 
-    return (
-        <div>
-            <div className='App'>
-                <h1>Gallery</h1>
-                <p>Eat My Shiny Metal Ass</p>
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/photos') //TODO: Change this to the actual api route when productionized
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setImages(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                <div className='App'>
+                    <h1>Gallery</h1>
+                    <p>Eat My Shiny Metal Ass</p>
+                </div>
+                <div style={{
+                    // display: 'flex', // This is causing pictures to disappear
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                >
+                    <Gallery images={images} enableImageSelection={false} margin={0}/>
+                </div>
             </div>
-            <div style={{
-                // display: 'flex', // This is causing pictures to disappear
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-            >
-                <Gallery images={IMAGES} enableImageSelection={false} margin={0}/>
-            </div>
-        </div>
 
-    );
+        );
+    }
 };

16172168f9b2450445dba8254252923f4e019b01
Brandon Sorrow <bsorrow@healthcarebluebook.com>
2021-10-18 20:21:41 -0500

    added some text to the about page

--- src/about.js
+++ src/about.js
@@ -2,17 +2,22 @@ import React from "react";
 
 const About = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-            <h1>GOOT</h1>
+        <div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <h1>Peter Swan aka Gomez Adams</h1>
+            </div>
+            <div align="center">
+                <p>Peter Swan is a tattoo apprentice at Grimoire Ink Tattoo Parlor in Gallatin, TN</p>
+            </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default About;

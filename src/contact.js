6ea4e6204e4c1f89e8e88de15219650aa493986b
Brandon Sorrow <bsorrow@healthcarebluebook.com>
2021-10-18 20:17:09 -0500

    simpler but kinda ugly contact page

--- src/contact.js
+++ src/contact.js
@@ -1,44 +1,15 @@
 import React from 'react'
 import './Contact.css'
-import {useForm} from "react-hook-form";
 
 function Contact() {
-    const {register, handleSubmit, watch, formState: {errors}} = useForm();
-    const message = watch('message') || "";
-    const messageCharsLeft = 1000 - message.length;
-    const onSubmit = data => console.log(data); // Here is where the API would get called to send an email
 
     return (
         <div>
             <h1>Contact Me</h1>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}
-            >
-
-                <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
-                    {errors.contact_name && errors.contact_name.type === "required" && (
-                        <div role="alert">Name is required<br/></div>
-                    )}
-                    <input type='text' name='contact_name'
-                           maxLength='100' {...register('contact_name', {required: true})} placeholder='Name'/>
-                    <br/>
-                    {errors.contact_email && errors.contact_email.type === "required" && (
-                        <div role="alert">Name is required<br/></div>
-                    )}
-                    <input type='email' name='contact_email' {...register('contact_email', {required: true})}
-                           placeholder='Email'/>
-                    <br/>
-                    <textarea name='message' maxLength='1000' {...register('message', {required: false})}
-                              placeholder='Message'/>
-                    <p className='message-chars-left'>Characters Remaining: {messageCharsLeft}</p>
-                    <br/>
-                    <input type='submit' value='Send'/>
-                </form>
-            </div>
+            <div class="row" align="center"><a class="text-center" href="mailto:peterswan.iu@gmail.com">Send me an
+                email</a></div>
+            <div className="row" align="center"><a href="">Visit the Shop!</a></div>
+            <div className="row" align="center"><a href="">Check out my Instagram</a></div>
         </div>
     );
 }
